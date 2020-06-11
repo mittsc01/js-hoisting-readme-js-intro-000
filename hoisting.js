635dfd462cfbeaa57cc2eb4a94e797c679c19048
Scott Mittman <sm2884@nau.edu>
2020-06-11 15:53:31 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,7 +3,7 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  
+
   var lyric = "maybe";
   return lyric;
 }
@@ -13,9 +13,9 @@ function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  
 
-  
+
+
   }
   thisIsCrazy();
 }
