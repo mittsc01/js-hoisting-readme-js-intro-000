475b39e97a40f909e93fd66f5a179378ae8e9348
Scott Mittman <sm2884@nau.edu>
2020-06-11 15:55:35 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -24,8 +24,9 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+    
   }
 
   sayMy();
